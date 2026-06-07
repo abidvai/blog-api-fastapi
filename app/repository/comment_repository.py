69c5993d43720a5718e08d2803abd4c93db80911
abdullah al abid <abdullahabid1660@gmail.com>
2026-06-07 11:50:06 +0600

    feat: comment

--- app/repository/comment_repository.py
+++ app/repository/comment_repository.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import func, select
+from sqlalchemy.orm import selectinload
 from app.models.comment_model import Comment
 
 
@@ -11,12 +12,20 @@ class CommentRepository:
         self.db.add(comment)
         await self.db.commit()
         await self.db.refresh(comment)
-        return comment
+        # Load user relation specifically for serialization
+        result = await self.db.execute(
+            select(Comment).where(Comment.id == comment.id).options(selectinload(Comment.user))
+        )
+        return result.scalar_one()
 
     async def update_comment(self, comment: Comment) -> Comment:
         await self.db.commit()
         await self.db.refresh(comment)
-        return comment
+        # Load user relation specifically for serialization after update
+        result = await self.db.execute(
+            select(Comment).where(Comment.id == comment.id).options(selectinload(Comment.user))
+        )
+        return result.scalar_one()
 
     async def delete_comment(self, comment_id: int) -> None:
         comment = await self.db.get(Comment, comment_id)
@@ -24,17 +33,20 @@ class CommentRepository:
         await self.db.commit()
 
     async def get_comment_by_id(self, comment_id: int) -> Comment | None:
-        return await self.db.get(Comment, comment_id)
+        result = await self.db.execute(
+            select(Comment).where(Comment.id == comment_id).options(selectinload(Comment.user))
+        )
+        return result.scalar_one_or_none()
 
     async def get_comments_by_blog_id(self, blog_id: int) -> list[Comment]:
         result = await self.db.execute(
-            select(Comment).where(Comment.post_id == blog_id)
+            select(Comment).where(Comment.post_id == blog_id).options(selectinload(Comment.user))
         )
         return result.scalars().all()
 
     async def get_comments_by_user_id(self, user_id: int) -> list[Comment]:
         result = await self.db.execute(
-            select(Comment).where(Comment.user_id == user_id)
+            select(Comment).where(Comment.user_id == user_id).options(selectinload(Comment.user))
         )
         return result.scalars().all()
 
